0966a718dfbe6506f43e0250fa304137aeeaceb1
Sergio Bianchi <ss.bianchi@gmail.com>
2020-09-08 23:21:15 -0300

    Add TextBox, StarDate, EndDate and Buttom

--- PBL_YahooFinance/ViewModels/LoadHistoricalPriceViewModel.cs
+++ PBL_YahooFinance/ViewModels/LoadHistoricalPriceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
+using PBL_YahooFinance.Commands;
 using PBL_YahooFinance.Models;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using YahooFinanceApi;
 
 namespace PBL_YahooFinance.ViewModels
@@ -16,6 +18,67 @@ namespace PBL_YahooFinance.ViewModels
     {
         #region Variables/Properties
         public ObservableCollection<HistoricalPriceModel> HistoricalPrice { get; set; }
+
+        public ICommand LoadHistoricalInfoAsyncCommand { get; set; }
+        string _loadAsset;
+        public string LoadAsset
+        {
+            get { return _loadAsset; }
+            set
+            {
+                if (_loadAsset == value)
+                    return;
+                _loadAsset = value;
+                RaisePropertyChanged(nameof(LoadAsset));
+            }
+        }
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get
+            {
+                return _statusMessage;
+            }
+            set
+            {
+                _statusMessage = value;
+                RaisePropertyChanged(nameof(StatusMessage));
+                OnPropertyChanged(nameof(HasStatusMessage));
+            }
+        }
+        public bool HasStatusMessage => !string.IsNullOrEmpty(StatusMessage);
+
+
+        DateTime _SelectedStartDate;
+        public DateTime SelectedStartDate
+        {
+            get { return _SelectedStartDate; }
+            set
+            {
+                if (_SelectedStartDate == value)
+                    return;
+                _SelectedStartDate = value;
+                RaisePropertyChanged(nameof(SelectedStartDate));
+                RaisePropertyChanged(nameof(SelectedEndDate));
+            }
+        }
+        public DateTime StartDate { get; set; }
+
+        DateTime _selectedEndDate;
+        public DateTime SelectedEndDate
+        {
+            get { return _selectedEndDate; }
+            set
+            {
+                if (_selectedEndDate == value)
+                    return;
+                _selectedEndDate = value;
+                RaisePropertyChanged(nameof(SelectedStartDate));
+                RaisePropertyChanged(nameof(SelectedEndDate));
+            }
+        }
+        public DateTime EndDate { get; set; }
+
         public SeriesCollection ChartPriceValues { get; set; }
         public string[] ChartDates { get; set; }
         public Func<double, string> YFormatter { get; set; }
@@ -25,6 +88,9 @@ namespace PBL_YahooFinance.ViewModels
         public LoadHistoricalPriceViewModel()
         {
             HistoricalPrice = new ObservableCollection<HistoricalPriceModel>();
+            LoadHistoricalInfoAsyncCommand = new AsyncRelayCommand(LoadHistoricalPrice, (ex) => StatusMessage = ex.Message);
+            StartDate = DateTime.Now.AddMonths(-1);
+            EndDate = DateTime.Now.Date;
         }
         #endregion
 
@@ -32,24 +98,46 @@ namespace PBL_YahooFinance.ViewModels
         public static LoadHistoricalPriceViewModel LoadViewModel(Action<Task> onLoaded = null)
         {
             LoadHistoricalPriceViewModel viewModel = new LoadHistoricalPriceViewModel();
-            
-            viewModel.Load().ContinueWith(t => onLoaded?.Invoke(t));
+
+            //viewModel.Load().ContinueWith(t => onLoaded?.Invoke(t));
 
             return viewModel;
         }
-        public async Task Load()
+        //public async Task Load()
+        //{
+        //"MGLU3.SA"
+        //var result = await Yahoo.GetHistoricalAsync("MGLU3.SA", new DateTime(2011, 05, 01), DateTime.Now, Period.Daily);
+
+        //HistoricalPrice.AddRange(result.Where(a => a.Close != 0).ToList().ConvertAll(a => PBL_YahooFinance.BLL.Converter.ItemToFileHelperDto(a)));
+
+        //var closePrice = result.Where(a => a.Close != 0).Select(a => a.Close).ToList();
+        //var dateArray = result.Where(a => a.Close != 0).Select(a => a.DateTime.ToString("MM/dd/yyyy")).ToArray();
+
+        //await LoadChart(closePrice, dateArray);
+
+        //RaiseAllCanExecuteChanged();
+        //}
+
+        private async Task LoadHistoricalPrice()
         {
-            var result = await Yahoo.GetHistoricalAsync("AAPL", new DateTime(2017, 1, 3), DateTime.Now, Period.Daily);
+            StatusMessage = "Status: Loading...";
+            var result = await Yahoo.GetHistoricalAsync(LoadAsset, StartDate, EndDate, Period.Daily);
 
-            HistoricalPrice.AddRange(result.ToList().ConvertAll(a => PBL_YahooFinance.BLL.Converter.ItemToFileHelperDto(a)));
+            HistoricalPrice.Clear();
+            HistoricalPrice.AddRange(result.Where(a => a.Close != 0)
+                                           .OrderByDescending(a => a.DateTime)
+                                           .ToList()
+                                           .ConvertAll(a => PBL_YahooFinance.BLL.Converter.ItemToFileHelperDto(a)));
 
-            var closePrice = result.Select(a => a.Close).ToList();
-            var dateArray = result.Select(a => a.DateTime.ToString("MM/dd/yyyy")).ToArray();
+            var closePrice = result.Where(a => a.Close != 0).Select(a => a.Close).ToList();
+            var dateArray = result.Where(a => a.Close != 0).Select(a => a.DateTime.ToString("MM/dd/yyyy")).ToArray();
 
             await LoadChart(closePrice, dateArray);
 
             RaiseAllCanExecuteChanged();
+            StatusMessage = "Status: Success.";
         }
+
         public async Task<bool> LoadChart(List<decimal> closePriceList, string[] dateArray)
         {
             ChartPriceValues = new SeriesCollection
@@ -70,9 +158,12 @@ namespace PBL_YahooFinance.ViewModels
         #endregion
         private void RaiseAllCanExecuteChanged()
         {
+            RaisePropertyChanged(nameof(StartDate));
+            RaisePropertyChanged(nameof(EndDate));
             RaisePropertyChanged(nameof(ChartPriceValues));
             RaisePropertyChanged(nameof(YFormatter));
             RaisePropertyChanged(nameof(ChartDates));
+            RaisePropertyChanged(nameof(StatusMessage));
         }
     }
 }
